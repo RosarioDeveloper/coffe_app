710f591e4206cb2552f445ce8125c3528be24e8e
rosariodeveloper <rosariomassango@gmail.com>
2022-07-23 23:31:10 +0100

    Set AppColors class

---
+++ lib/shared/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xffD17842);
+  static const Color secondary = Color(0xFF40474F);
+  static const Color dark = Color(0xFF0C0F14);
+  static const Color dark80 = Color(0xFF141921);
+  static const Color dark50 = Color(0xFF3E4652);
+  static const Color dark30 = Color(0xFF76859C);
+}
