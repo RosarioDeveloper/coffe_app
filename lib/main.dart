c235407af2feaf192b89a93fec3ef92f5ca9e3c1
rosariodeveloper <rosariomassango@gmail.com>
2022-07-29 18:57:28 +0100

    Move app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:app1/config/app.dart';
+import 'package:app1/shared/app.dart';
 import 'package:app1/screens/home.dart';
 import 'package:app1/shared/colors.dart';
 import 'package:flutter/material.dart';
