140065ac65b67ae2c47f4ea8152bca492e5777b2
rosariodeveloper <rosariomassango@gmail.com>
2022-07-29 19:06:58 +0100

    Move

---
+++ lib/shared/widgets/search_input.dart
@@ -0,0 +1,19 @@
+import 'package:app1/shared/app.dart';
+import 'package:flutter/material.dart';
+
+class SearchInput extends StatelessWidget {
+  const SearchInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.search,
+          //color: Colors.grey,
+        ),
+        hintText: 'Find your coffe',
+      ),
+    );
+  }
+}
