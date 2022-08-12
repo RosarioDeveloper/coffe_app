09129d428cc59904a25f29c169ab94deae707f19
rosariodeveloper <rosariomassango@gmail.com>
2022-08-12 21:52:19 +0100

    Feat: build component

---
+++ lib/screens/splash/splash.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:app1/shared/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 4),
+      () {
+        Navigator.of(context).pushNamed('/home');
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              AppColors.primary,
+              AppColors.primary,
+            ],
+          ),
+        ),
+        child: Center(
+          child: AnimatedTextKit(
+            animatedTexts: [
+              WavyAnimatedText(
+                'Coffee'.toUpperCase(),
+                textStyle: const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ],
+            isRepeatingAnimation: true,
+            repeatForever: true,
+            onTap: () {
+              print("Tap Event");
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
