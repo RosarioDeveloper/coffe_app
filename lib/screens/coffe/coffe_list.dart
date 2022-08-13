5e760c6e6118af5411a3d940cbacc8135e6baf37
rosariodeveloper <rosariomassango@gmail.com>
2022-08-13 13:47:54 +0100

    Chore: Add navigation to detail page

--- lib/screens/coffe/coffe_list.dart
+++ lib/screens/coffe/coffe_list.dart
@@ -1,22 +1,49 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace, must_be_immutable
 
-import 'package:app1/models/coffe_model.dart';
+import 'package:app1/screens/coffe/coffe_model.dart';
 import 'package:app1/screens/coffe/coffe_controller.dart';
 import 'package:app1/screens/coffe/widget/coffe_item_widget.dart';
 import 'package:app1/shared/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class CoffeList extends StatelessWidget {
-  CoffeController _coffeController = CoffeController();
-
+class CoffeList extends StatefulWidget {
   CoffeList({Key? key}) : super(key: key);
+
+  @override
+  State<CoffeList> createState() => _CoffeListState();
+}
+
+class _CoffeListState extends State<CoffeList> {
+  final _coffeController = CoffeController();
+  late List<CoffeModel> _coffeList = [];
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+
+    _coffeController.list().then((data) {
+      setState(() {
+        _loading = false;
+        _coffeList = data;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     //final size = MediaQuery.of(context).size;
 
+    if (_loading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
     return DefaultTabController(
-      length: _coffeController.coffeList.length,
+      length: _coffeList.length,
       child: Column(
         children: [
           TabBar(
@@ -37,25 +64,29 @@ class CoffeList extends StatelessWidget {
             labelStyle: TextStyle(color: Colors.red),
             labelColor: AppColors.primary,
             unselectedLabelColor: AppColors.dark30,
-            tabs: _coffeController.coffeList.map((item) {
-              return Tab(
-                text: item['title'],
-              );
+            tabs: _coffeList.map((item) {
+              return Tab(text: item.category);
             }).toList(),
           ),
           SizedBox(height: 5),
           Expanded(
             child: TabBarView(
               children: [
-                for (final item in _coffeController.coffeList)
+                for (final coffe in _coffeList)
                   ListView.builder(
                     scrollDirection: Axis.horizontal,
-                    itemCount: item['items'].length,
+                    itemCount: coffe.items.length,
                     itemBuilder: (context, index) {
-                      return CoffeItem(
-                        coffe: CoffeModelItem.fromJson(
-                          item['items'][index],
+                      return GestureDetector(
+                        child: CoffeItem(
+                          coffe: coffe.items[index],
                         ),
+                        onTap: () {
+                          GoRouter.of(context).go(
+                            '/coffe/details',
+                            extra: {'data': coffe.items[index]},
+                          );
+                        },
                       );
                     },
                   ),
