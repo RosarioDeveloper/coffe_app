7583e0f8268e6f88ec41d9d6cdc8db475a882c0f
rosariodeveloper <rosariomassango@gmail.com>
2022-08-13 13:54:42 +0100

    Chore: endpoint list

--- lib/screens/coffe/coffe_controller.dart
+++ lib/screens/coffe/coffe_controller.dart
@@ -1,121 +1,16 @@
-import 'package:app1/models/coffe_model.dart';
+import 'package:app1/data/coffe_data.dart';
+import 'package:app1/screens/coffe/coffe_model.dart';
 
 class CoffeController {
-  List coffeList = [
-    {
-      'title': 'Cappucino',
-      'items': [
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 3.20,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 2.20,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 1.20,
-          'description': 'With Milk',
-        }
-      ],
-    },
-    {
-      'title': 'Expresso',
-      'items': [
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.11,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.05,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        }
-      ],
-    },
-    {
-      'title': 'Late',
-      'items': [
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        }
-      ],
-    },
-    {
-      'title': 'Flat with Milk',
-      'items': [
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        }
-      ],
-    },
-    {
-      'title': 'Flat with Milk',
-      'items': [
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        },
-        {
-          'images': 'coffe1.jpeg',
-          'name': 'Cappucino',
-          'price': 4.20,
-          'description': 'With Milk',
-        }
-      ],
-    },
-  ];
+  Future<List<CoffeModel>> list() {
+    return Future.delayed(const Duration(seconds: 2), () {
+      var list = coffeData
+          .map(
+            (item) => CoffeModel.fromJson(item),
+          )
+          .toList();
 
-  /* List<CoffeModel> getCoffeList() {
-    List<CoffeModel> coffeList = [];
-
-    for (var coffe  in coffeList ) {
-      coffeList.add(Coffe(
-        title: this.coffeList[i]['title'],
-        items: this.coffeList[i]['items'],
-      ));
-    }
-    return coffeList;
-  } */
+      return list;
+    });
+  }
 }
