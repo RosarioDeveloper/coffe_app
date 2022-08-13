404ec7728052e76237ea4d0277e91e35a0d7960b
rosariodeveloper <rosariomassango@gmail.com>
2022-08-13 13:47:12 +0100

    Chore: Setting go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,57 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:app1/shared/app.dart';
+import 'package:app1/screens/coffe/coffe_details.dart';
+import 'package:app1/screens/splash/splash.dart';
+import 'package:app1/shared/theme.dart';
 import 'package:app1/screens/home.dart';
-import 'package:app1/shared/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return SplashPage();
+        },
+      ),
+      GoRoute(
+        path: '/home',
+        builder: (BuildContext context, GoRouterState state) {
+          return HomePage();
+        },
+      ),
+      GoRoute(
+        path: '/coffe/details',
+        builder: (BuildContext context, GoRouterState state) {
+          final extra = state.extra as dynamic;
+
+          return CoffeDetails(
+            key: state.pageKey,
+            coffe: extra != null ? extra['data'] : null,
+          );
+        },
+      ),
+    ],
+  );
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      home: Container(
-        decoration: BoxDecoration(
-          color: AppColors.dark,
-        ),
-        child: HomePage(),
-      ),
       theme: App.theme(),
+      routeInformationProvider: _router.routeInformationProvider,
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      //initialRoute: '/splash',
     );
   }
 }
