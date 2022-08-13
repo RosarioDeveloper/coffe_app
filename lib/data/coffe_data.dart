11633ac4fbc1a10315147954eb537a7c2cfa7d14
rosariodeveloper <rosariomassango@gmail.com>
2022-08-13 15:08:52 +0100

    Feat: Provider data

---
+++ lib/data/coffe_data.dart
@@ -0,0 +1,88 @@
+List coffeData = [
+  {
+    'category': 'Cappucino',
+    'items': [
+      {
+        'image': 'coffe1.jpeg',
+        'name': 'Cappucino',
+        'price': 4.20,
+        'description': 'With Milk',
+      },
+      {
+        'image': 'coffe3.jpeg',
+        'name': 'Cappucino',
+        'price': 3.20,
+        'description': 'Normal',
+      },
+      {
+        'image': 'coffe3.jpeg',
+        'name': 'Cappucino',
+        'price': 2.20,
+        'description': 'With Milk',
+      },
+      {
+        'image': 'coffe4.jpeg',
+        'name': 'Cappucino',
+        'price': 1.20,
+        'description': 'With Milk',
+      }
+    ],
+  },
+  {
+    'category': 'Expresso',
+    'items': [
+      {
+        'image': 'coffe1.jpeg',
+        'name': 'Expresso',
+        'price': 4.11,
+        'description': 'With Milk',
+      },
+      {
+        'image': 'coffe3.jpeg',
+        'name': 'Expresso',
+        'price': 4.05,
+        'description': 'With Milk',
+      },
+      {
+        'image': 'coffe2.webp',
+        'name': 'Expresso',
+        'price': 4.20,
+        'description': 'With Milk',
+      }
+    ],
+  },
+  {
+    'category': 'Late',
+    'items': [
+      {
+        'image': 'coffe4.jpeg',
+        'name': 'Late',
+        'price': 4.20,
+        'description': 'With Milk',
+      },
+      {
+        'image': 'coffe1.jpeg',
+        'name': 'Late',
+        'price': 4.20,
+        'description': 'With Milk',
+      }
+    ],
+  },
+  {
+    'category': 'Flat with Milk',
+    'items': [
+      {
+        'image': 'coffe2.webp',
+        'name': 'Flat',
+        'price': 4.20,
+        'description': 'With Milk',
+      },
+      {
+        'image': 'coffe3.jpeg',
+        'name': 'Flat',
+        'price': 4.20,
+        'description': 'With Milk',
+      }
+    ],
+  },
+];
