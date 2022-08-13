2ca7aa3b5c19418e83d23e226bbba62c4dbdef09
rosariodeveloper <rosariomassango@gmail.com>
2022-08-13 13:01:40 +0100

    Fix: navigator router

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -3,8 +3,8 @@ import 'dart:ui';
 
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:app1/shared/colors.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashPage extends StatefulWidget {
   SplashPage({Key? key}) : super(key: key);
@@ -21,7 +21,7 @@ class _SplashPageState extends State<SplashPage> {
     Future.delayed(
       const Duration(seconds: 4),
       () {
-        Navigator.of(context).pushNamed('/home');
+        GoRouter.of(context).go('/home');
       },
     );
   }
