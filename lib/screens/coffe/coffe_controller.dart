ef520230c862779df4b9c33013f13bb7645f7bad
rosariodeveloper <rosariomassango@gmail.com>
2022-08-13 14:06:34 +0100

    Chore: change second delay on future

--- lib/screens/coffe/coffe_controller.dart
+++ lib/screens/coffe/coffe_controller.dart
@@ -3,7 +3,7 @@ import 'package:app1/screens/coffe/coffe_model.dart';
 
 class CoffeController {
   Future<List<CoffeModel>> list() {
-    return Future.delayed(const Duration(seconds: 2), () {
+    return Future.delayed(const Duration(seconds: 1), () {
       var list = coffeData
           .map(
             (item) => CoffeModel.fromJson(item),
